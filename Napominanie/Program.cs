5c43e7146d0a447db7f7f1b2717ba9cdee258823
Денис Егорычев <den.egorychev@hotmail.com>
2017-10-31 01:32:56 +0400

    2 задача. Фикс первой

--- Napominanie/Program.cs
+++ Napominanie/Program.cs
@@ -10,24 +10,21 @@ namespace Napominanie
     {
         static void Main(string[] args)
         {
-            DateTime development = DateTime.Parse(Console.ReadLine());
+            string[] str1 = Console.ReadLine().Split(' ', ':', '.');
+            DateTime development = new DateTime(int.Parse(str1[2]), int.Parse(str1[1]), int.Parse(str1[0]), int.Parse(str1[3]), int.Parse(str1[4]), 0);
             int n = int.Parse(Console.ReadLine());
 
-            List<string[]> napominaniya = new List<string[]>();
-
             for (int i = 0; i < n; i++)
-                napominaniya.Add(Console.ReadLine().Split(' '));
-
-            for(int i=0; i<n; i++)
             {
-                int d = int.Parse(napominaniya[i][0]);
-                int h = int.Parse(napominaniya[i][1]);
-                int m = int.Parse(napominaniya[i][2]);
+                string[] str = Console.ReadLine().Split(' ');
+
+                int d = int.Parse(str[0]);
+                int h = int.Parse(str[1]);
+                int m = int.Parse(str[2]);
 
                 DateTime newdate = development.AddDays(-d).AddHours(-h).AddMinutes(-m);
                 Console.WriteLine(newdate.ToString("dd.MM.yyyy HH:mm"));
             }
-
             Console.ReadKey();
         }
     }
--- Ping-Ping/Program.cs
+++ Ping-Ping/Program.cs
@@ -10,7 +10,16 @@ namespace Ping_Ping
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            long a = 25, b = 629856, mod = 1000000009, A = 1, B = 1;
 
+            for (int i = 0; i < n; i++)
+            {
+                A = (A * a) % mod;
+                B = (B * b) % mod;
+            }
+            Console.WriteLine(A + " " + B);
+            Console.ReadKey();
         }
     }
 }
