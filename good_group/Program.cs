ca8785d334b0c8fd4f0e24735a9d5f0d16916226
Денис Егорычев <den.egorychev@hotmail.com>
2017-10-31 21:05:22 +0400

    Рабочее, но не оптимальное решение третей задачи

--- good_group/Program.cs
+++ good_group/Program.cs
@@ -10,71 +10,43 @@ namespace good_group
     {
         static void Main(string[] args)
         {
-            string par = "9 2";
-            int k = int.Parse(par.Split(' ')[1]);
-            string input = "5 1 1 1 2 2 2 9 9";
-
-            int count = 0;
-            string[] ms = input.Split(' ');
-            int SubStringCount = ms.Count();
-            String bstr = "";
-            int perDel = ms.Length + 1;
-            do
+            int k = int.Parse(Console.ReadLine().Split(' ')[1]);
+            string input = Console.ReadLine();
+            List<int> segment = input.Split(' ').Select(int.Parse).ToList();
+            int Counter = 0;
+            while(segment.Count>0)
             {
-                perDel--;
-                int h = 0;
-                bstr = "";
-
-                int j = 0;
-                for (int ii = 0; ii < SubStringCount; ii++)
+                List<int> section = segment.ToList();
+                while(section.Count>0)
                 {
-                    h++;
-                    bstr += ms[ii] + " ";
-                    if (h == perDel)
-                    {
-                        bstr = bstr.Trim();
-                        ++j;
-                        if (iftrue(bstr, k))
-                            count++;
-                        //Console.WriteLine(bstr);
+                    Console.Write(string.Join(" ", section));
 
-                        bstr = "";
-                        h = 0;
-                        ii = ii - perDel + 1;
+                    if (SmashList(section) <= k)
+                    {
+                        Counter++;
+                        Console.WriteLine(" +++");
                     }
+                    else
+                        Console.WriteLine();
+
+                    section.RemoveAt(section.Count - 1);
                 }
-            } while (perDel > 1);
-            Console.WriteLine(count);
+                segment.RemoveAt(0);
+            }
+
+            Console.WriteLine(Counter);
             Console.ReadKey();
         }
-
-        static bool iftrue(string input, int k)
+        static int SmashList (List<int> inputList)
         {
-            List<int> workint = input.Split(' ').Select(int.Parse).ToList();
-            Console.Write(input+ " || ");
-            workint.Sort();
-            //Console.WriteLine();
-            foreach (int i in workint)
-                Console.Write(i + " ");
-
-            int count = 0;
-            int memory = 0;
-            foreach (int i in workint)
+            int maxcount = 0;
+            foreach (int i in inputList.Distinct())
             {
-                if (i == memory)
-                    count++;
-                else
-                    count = 0;
-
-                if (count == k)
-                {
-                    Console.WriteLine("||---");
-                    return false;
-                }
-                memory = i;
+                int count = inputList.Count(x => x == i);
+                if (count > maxcount)
+                    maxcount = count;
             }
-            Console.WriteLine("||+++");
-            return true;
+            return maxcount;
         }
     }
 }
---
+++ test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "5 1 1 1 2 2 2 9 9";
+
+            char[] ch = file.ToCharArray().Distinct().ToArray();
+            for (int i = 0; i < ch.Length; i++)
+                if (ch[i] != ' ')
+                    Console.WriteLine("Символ: |{0}| Число повторений: |{1}|", ch[i], file.ToCharArray().Count(x => x == ch[i]));
+
+            Console.ReadKey();
+        }
+    }
+}
